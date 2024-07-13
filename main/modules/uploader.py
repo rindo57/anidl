e9396b20c909b5c4065dfad07a5c9f1b19d7fa2f
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2024-07-13 16:34:45 +0530

    Update uploader.py

--- main/modules/uploader.py
+++ main/modules/uploader.py
@@ -67,8 +67,8 @@ async def upload_video(msg: Message, title, tito, img, file, id, tit, name, ttl,
             source = extract_source(filed)
             print('filed: ', filed)
             anidltitle = filed.replace("[AniDL] ", "")
-            anidltitle = anidltitle.replace("[Web][480p x265 10Bit][Opus][Erai-raws].mkv", "")
-            
+            anidltitle = anidltitle.replace(" [Web][480p x265 10Bit][Opus][Erai-raws].mkv", "")
+            anidltitle = anidltitle.replace(" [Web][480p x265 10Bit][Opus][SubsPlease].mkv", "")
             fukpath = "downloads/" + filed
             caption = f"{filed}"
 
@@ -195,7 +195,8 @@ async def upload_video720p(msg: Message, title, img, file, id, tit, name, ttl, m
             source = extract_source(filed)
             print('filed: ', filed)
             anidltitle = filed.replace("[AniDL] ", "")
-            anidltitle = anidltitle.replace("[Web][720p x265 10Bit][Opus][Erai-raws].mkv", "")
+            anidltitle = anidltitle.replace(" [Web][720p x265 10Bit][Opus][Erai-raws].mkv", "")
+            anidltitle = anidltitle.replace(" [Web][720p x265 10Bit][Opus][SubsPlease].mkv", "")
             filed = filed.replace("[1080p Web-DL]", "[Web][720p x265 10Bit][Opus][Erai-raws]")
             fukpath = "downloads/" + filed
             caption = f"{filed}"
@@ -316,6 +317,7 @@ async def upload_video1080p(msg: Message, title, img, file, id, tit, name, ttl, 
             print('filed: ', filed)
             anidltitle = filed.replace("[AniDL] ", "")
             anidltitle = anidltitle.replace(" [Web][1080p x265 10Bit][AAC][Erai-raws].mkv", "")
+            anidltitle = anidltitle.replace(" [Web][1080p x265 10Bit][AAC][Subsplease].mkv", "")
             filed = filed.replace("[1080p Web-DL]", "[Web][1080p x265 10Bit][AAC][Erai-raws]")
             fukpath = "downloads/" + filed
             caption = f"{filed}"
