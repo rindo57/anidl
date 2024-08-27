1bbb06811b5b2eb236d807f0846581c8756ef00d
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2024-08-27 18:25:45 +0530

    Update uploader.py

--- main/modules/uploader.py
+++ main/modules/uploader.py
@@ -87,6 +87,7 @@ async def upload_video(msg: Message, title, tito, img, file, id, tit, name, ttl,
             print("hash1:", hash)
             flx = filed.replace(".mkv", "")
             filedx = flx + " [" + "".join([random.choice(ascii_letters.upper() + digits) for n in range(8)]) + "]"
+            print(filedx)
             save_file_in_db(filedx, hash, subtitle, img, audio_info, tit, alink, size480p, upid)
             print(hash)
             ddlurl = f"https://anidl.ddlserverv1.me.in/beta/{hash}"
@@ -217,7 +218,10 @@ async def upload_video720p(msg: Message, title, tito, img, file, id, tit, name, 
             )
             await asyncio.sleep(3)
             hash = "".join([random.choice(ascii_letters + digits) for n in range(50)])
-            save_file_in_db(filed, hash, subtitle, img, audio_info, tit, alink, size720p, upid)
+            flx = filed.replace(".mkv", "")
+            filedx = flx + " [" + "".join([random.choice(ascii_letters.upper() + digits) for n in range(8)]) + "]"
+            print(filedx)
+            save_file_in_db(filedx, hash, subtitle, img, audio_info, tit, alink, size720p, upid)
             print(hash)
             ddlurl = f"https://anidl.ddlserverv1.me.in/beta/{hash}"
             gcaption = f"`📺 {filed}`\n\n`🔗 EP - {ep_num}:  https://anidl.ddlserverv1.me.in/beta/{hash}`" + "\n\n" + f"🔠 __{tit}__" + "\n" + "\n" + f"📝 `{subtitle}`"
@@ -339,7 +343,10 @@ async def upload_video1080p(msg: Message, title, tito, img, file, id, tit, name,
             )
             await asyncio.sleep(3)
             hash = "".join([random.choice(ascii_letters + digits) for n in range(50)])
-            save_file_in_db(filed, hash, subtitle, img, audio_info, tit, alink, size1080p, upid)
+            flx = filed.replace(".mkv", "")
+            filedz = flx + " [" + "".join([random.choice(ascii_letters.upper() + digits) for n in range(8)]) + "]"
+            print(filedz)
+            save_file_in_db(filedz, hash, subtitle, img, audio_info, tit, alink, size1080p, upid)
             print(hash)
             ddlurl = f"https://anidl.ddlserverv1.me.in/beta/{hash}"
             gcaption = f"`📺 {filed}`\n\n`🔗 EP - {ep_num}:  https://anidl.ddlserverv1.me.in/beta/{hash}`" + "\n\n" + f"🔠 __{tit}__" + "\n" + "\n" + f"📝 `{subtitle}`"
